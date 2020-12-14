6b1fa8a57be6ca2ec411b2fa5df48293eb45f212
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-12-14 23:06:02 +0530

    [Abhishek] added to the UC2 branch

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using EmployeePayrollMVC.Models.Common;
+using EmployeePayrollMVC.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeePayrollMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeRepository employeeRepository = new EmployeeRepository();
+        public ActionResult Index()
+        {
+            List<EmployeeViewModel> list = employeeRepository.GetEmployees();
+            return View(list);
+        }
+    }
+}
---
+++ Models/Common/EmployeeViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EmployeePayrollMVC.Models.Common
+{
+    public class EmployeeViewModel
+    {
+        public int empid { get; set; }
+
+        public string name { get; set; }
+
+        public string gender { get; set; }
+
+        public int deptid { get; set; }
+
+        public string department { get; set; }
+
+        public string amount { get; set; }
+
+        public int salaryid { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MMM/yyyy}")]
+        public DateTime startdate { get; set; }
+
+        public string description { get; set; }
+    }
+}
